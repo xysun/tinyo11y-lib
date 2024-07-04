f1f1566f918ec642958c823a88e476a8ca4a91c7
xysun <xiayunsun@gmail.com>
2024-07-04 23:22:51 +0100

    only do info and error

--- lib.go
+++ lib.go
@@ -13,12 +13,16 @@ type TinyLogger struct {
 	LogPort string
 }
 
-func (logger *TinyLogger) Info(msg string) {
-	payload := map[string]string{"message": msg, "level": "Info"}
+func (logger *TinyLogger) output(level string, msg string) {
+	payload := map[string]string{"message": msg, "level": level}
 
 	go func() {
 		requestJson, _ := json.Marshal(payload)
 		http.Post(fmt.Sprintf("http://%s:%s/v1/log/%s", logger.LogHost, logger.LogPort, logger.ApiKey),
 			"application/json", bytes.NewBuffer(requestJson))
 	}()
+}
+
+func (logger *TinyLogger) Info(msg string) {
+	logger.output("info", msg)
 }
