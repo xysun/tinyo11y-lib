de4b8f4d9ce02463ad2b5badb28fe96399c1aaa8
xysun <xiayunsun@gmail.com>
2024-08-02 19:43:27 +0100

    support writing metric

--- lib.go
+++ lib.go
@@ -8,9 +8,9 @@ import (
 )
 
 type TinyLogger struct {
-	ApiKey  string
-	LogHost string
-	LogPort string // TODO: remove when launch
+	ApiKey string
+	Host   string
+	Port   string // TODO: remove when launch
 }
 
 func (logger *TinyLogger) output(level string, msg string) {
@@ -19,7 +19,7 @@ func (logger *TinyLogger) output(level string, msg string) {
 	go func() {
 		requestJson, _ := json.Marshal(payload)
 		// TODO: change to https when launch
-		http.Post(fmt.Sprintf("http://%s:%s/v1/log/%s", logger.LogHost, logger.LogPort, logger.ApiKey),
+		http.Post(fmt.Sprintf("http://%s:%s/v1/log/%s", logger.Host, logger.Port, logger.ApiKey),
 			"application/json", bytes.NewBuffer(requestJson))
 	}()
 }
@@ -30,5 +30,17 @@ func (logger *TinyLogger) Info(msg string) {
 
 func (logger *TinyLogger) Error(msg string, er error) {
 	// by default golang does not show stacktrace....
-	logger.output("info", fmt.Sprintf("%s: %s", msg, er.Error()))
+	logger.output("error", fmt.Sprintf("%s: %s", msg, er.Error()))
+}
+
+func (logger *TinyLogger) Metric(metricName string, metricValue float64) {
+	payload := map[string]string{"metric_name": metricName, "metric_value": fmt.Sprintf("%.6f", metricValue)}
+
+	go func() {
+		requestJson, _ := json.Marshal(payload)
+		// TODO: change to https when launch
+		http.Post(fmt.Sprintf("http://%s:%s/v1/metric/%s", logger.Host, logger.Port, logger.ApiKey),
+			"application/json", bytes.NewBuffer(requestJson))
+
+	}()
 }
